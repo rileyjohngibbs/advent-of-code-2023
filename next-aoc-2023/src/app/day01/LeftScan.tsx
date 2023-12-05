4d293741a925400912d82d7299026c39d6b6d1f9
Riley John Gibbs <rileyjohngibbs@gmail.com>
2023-12-04 22:08:19 -0800

    Add intermediate steps for scans

--- next-aoc-2023/src/app/day01/LeftScan.tsx
+++ next-aoc-2023/src/app/day01/LeftScan.tsx
@@ -1,13 +1,43 @@
+import { useState, useEffect } from "react"
+
 interface Result { line: string, leftDigit: number }
 interface Props {
     lines: string[], nextStateSetter: {(value: Result[]): void}
 }
 
 export default function LeftScan({ lines, nextStateSetter }: Props) {
-    const results = lines.map((line) => {
-        const digit = line.split("").find(s => !isNaN(parseInt(s)))
-        return { line, leftDigit: digit ? parseInt(digit) : -1}
+    const [rows, setRows] = useState<{counter: number, digit: number | null}[]>(
+        Array(lines.length).fill(null).map(() => {return {counter: 0, digit: null}})
+    )
+    useEffect(() => {
+        if (rows.findIndex(row => row.digit === null) === -1) {
+            setTimeout(() => nextStateSetter(rows.map(
+                ({digit}, i) => {return {line: lines[i], leftDigit: digit ?? 0}}
+            )), 2000)
+        } else {
+            const newRows: {counter: number, digit: number | null}[] = Array(lines.length)
+            lines.forEach((line, i) => {
+                const row = rows[i]
+                if (row.counter === null) {
+                    newRows[i] = row
+                } else {
+                    const parsed = parseInt(line[row.counter])
+                    if (!isNaN(parsed)) {
+                        newRows[i] = {counter: row.counter, digit: parsed}
+                    } else {
+                        newRows[i] = {counter: row.counter + 1, digit: null}
+                    }
+                }
+            })
+            setTimeout(() => setRows(newRows), 100)
+        }
+    }, [rows])
+
+    return rows.map(({counter, digit}, i) => {
+        return <div key={i}>{digit ?? "_"} {
+            lines[i].split("").map(
+                (c, j) => j === counter ? <span key="j" className="font-bold">{c}</span> : c
+            )
+        }</div>
     })
-    setTimeout(() => nextStateSetter(results), 2000)
-    return results.map(({line, leftDigit}, i) => <div key={i}>{leftDigit} {line}</div>)
 }
--- next-aoc-2023/src/app/day01/RightScan.tsx
+++ next-aoc-2023/src/app/day01/RightScan.tsx
@@ -1,15 +1,41 @@
+import { useState, useEffect } from "react"
+
 interface Result { line: string, leftDigit: number, rightDigit: number }
 interface Props {
     leftScannedLines: {line: string, leftDigit: number}[], nextStateSetter: {(value: Result[]): void}
 }
 
 export default function RightScan({ leftScannedLines, nextStateSetter }: Props) {
-    const results = leftScannedLines.map(({ line, leftDigit }) => {
-        const digit = line.split("").reverse().find(s => !isNaN(parseInt(s)))
-        return { line, leftDigit, rightDigit: digit ? parseInt(digit) : -1}
-    })
-    setTimeout(() => nextStateSetter(results), 2000)
-    return results.map(
-        ({line, leftDigit, rightDigit}, i) => <div key={i}>{leftDigit} {line} {rightDigit}</div>
+    const [rows, setRows] = useState<{counter: number, digit: number | null}[]>(
+        Array(leftScannedLines.length).fill(null).map(() => {return {counter: 1, digit: null}})
     )
+    useEffect(() => {
+        if (rows.findIndex(({digit}) => digit === null) === -1) {
+            nextStateSetter(rows.map((row, i) => {
+                const {line, leftDigit} = leftScannedLines[i]
+                return {line: line, leftDigit: leftDigit, rightDigit: row.digit ?? 0}
+            }))
+        } else {
+            const newRows: {counter: number, digit: number | null}[] = Array(leftScannedLines.length)
+            rows.forEach((row, i) => {
+                const {line} = leftScannedLines[i]
+                const parsed = parseInt(line[line.length - row.counter])
+                if (!isNaN(parsed)) {
+                    newRows[i] = {counter: row.counter, digit: parsed}
+                } else {
+                    newRows[i] = {counter: row.counter + 1, digit: null}
+                }
+            })
+            setTimeout(() => setRows(newRows), 100)
+        }
+    }, [rows])
+
+    return rows.map(({counter, digit}, i) => {
+        const {line, leftDigit} = leftScannedLines[i]
+        return <div key={i}>{leftDigit}{digit ?? "_"} {
+            line.split("").map(
+                (c, j) => j === line.length - counter ? <span key="j" className="font-bold">{c}</span> : c
+            )
+        }</div>
+    })
 }
