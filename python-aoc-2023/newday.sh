7cc0921b9e56a463eb462abf563a4af0ae511e7a
Riley John Gibbs <rileyjohngibbs@gmail.com>
2023-12-05 22:56:54 -0800

    Improve newday script

--- python-aoc-2023/newday.sh
+++ python-aoc-2023/newday.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 DAY=$(printf "%02d" $1)
+
 SOLUTION_DIR=$(dirname $BASH_SOURCE)/aoc/solutions/day$DAY
 mkdir $SOLUTION_DIR
 touch $SOLUTION_DIR/__init__.py
-sed "s/DAY = 0/DAY = $1/g" $(dirname $BASH_SOURCE)/aoc/template.py > $SOLUTION_DIR/alpha.py
+sed "s/DAY = 0/DAY = $1/g" $(dirname $BASH_SOURCE)/aoc/template.txt > $SOLUTION_DIR/alpha.py
 
 TEST_DIR=$(dirname $BASH_SOURCE)/aoc/tests/day$DAY
 mkdir $TEST_DIR
 touch $TEST_DIR/__init__.py
-touch $TEST_DIR/test_alpha.py
+sed "s/day00/day$DAY/g" $(dirname $BASH_SOURCE)/aoc/test_template.txt > $TEST_DIR/test_alpha.py
